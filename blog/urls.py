9d7d63dc3a6f94c282857786f281fd4bf31eccd1
itspooya <fallenangel201190@gmail.com>
2020-04-19 21:16:46 +0430

    :bug: fixed bug on

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,17 @@
 from . import views
 from django.urls import path
+from django.conf.urls import url
+
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+
+    url(r'^representation/', views.Representation),
+    url(r'^about/$', views.About),
+    url(r'^govash/$', views.govash),
+    url(r'^acleryc/$', views.acleryc),
+    url(r'^catalog/$', views.catalog),
+    url(r'^abouten/$',views.abouten),
+    url(r'^indexen/',views.indexen),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,39 @@
 from django.views import generic
 from .models import Post
+from django.shortcuts import render, redirect
+
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
 
+
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
+def Representation(request):
+    return render(request, "Representation.html")
+
+
+def About(request):
+    return render(request, "about.html")
+
+
+def govash(request):
+    return render(request, "govash.html")
+
+
+def acleryc(request):
+    return render(request, "acleryc.html")
+
+
+def catalog(request):
+    return render(request, "catalog.html")
+
+def abouten(request):
+    return render(request,"about_en.html")
+
+def indexen(request):
+    return render(request,"index_en.html")
